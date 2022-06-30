7212dfd9c8db1afd8f5ed9683e78b891a889d41a
roggergolden <77041053@certus.edu.pe>
2022-06-30 17:03:08 -0500

    js2

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -8,7 +8,53 @@ console.log(heading);
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll(".navegacion a")
+
+enlaces[0].textContent = "Nuevo texto para enlace";
+enlaces[0].classList.add("Nueva-clase");
+// enlaces[0].classList.remove("navegación__enlace")
+
+// Generar enlace con código de JavaScript
+
+const nuevoEnlace = document.createElement("A")
+
+//Agregar el href
+
+nuevoEnlace.href = "nuevo-enlace.html";
+
+//Agregar el texto
+
+nuevoEnlace.textContent = "Tienda Virtual";
+
+//Agregar la clase
+
+nuevoEnlace.classList.add("navegacion__enlace");
+
+//Agregarlo al documento
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+console.log(nuevoEnlace);
+
+// Eventos 
+
+console.log(1);
+
+window.addEventListener("load", function(){
+    
+console.log(2);
+
+});
+
+window.onload = function() {
+    console.log(3)
+}
 
 
 
-//getElementByid
+console.log(5);
+
+
+
