5abccd50f0bba968549adf1dd412098beffc8696
roggergolden <77041053@certus.edu.pe>
2022-06-30 15:19:01 -0500

    json

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,14 @@
+// querySelector 
+
+const heading = document.querySelector( ".header__texto h2") // 0 o 1 elementos 
+heading.textContent = "Nuevo Heading";
+
+console.log(heading);
+
+
+// querySelectorAll
+
+
+
+
+//getElementByid
