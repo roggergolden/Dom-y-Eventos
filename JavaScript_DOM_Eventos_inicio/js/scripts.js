3618ef935d5168962d76b183dde273de5dd78e40
roggergolden <77041053@certus.edu.pe>
2022-06-30 18:25:40 -0500

    js4

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -40,21 +40,112 @@ console.log(nuevoEnlace);
 
 // Eventos 
 
-console.log(1);
+// console.log(1);
 
-window.addEventListener("load", function(){
+// window.addEventListener("load", function(){ // load espera a que se descargue el JS y los archivos del HTML estén listos
     
-console.log(2);
+// console.log(2);
 
-});
+// });
 
-window.onload = function() {
-    console.log(3)
+// window.onload = function() {
+//     console.log(3);
+// }
+
+// document.addEventListener('DOMContentLoaded', function() { // Sólo espera por el HTML, pero no espera en CSS o imagenes
+//     console.log(4);
+// });
+
+// console.log(5);
+
+// window.onscroll = function(evento) {
+//     console.log(evento)
+// }
+
+//Seleccionar elementos y asociarles un evento
+
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault();
+
+//     //Validar un formulario
+
+//     console.log("enviando formularios")
+// });
+
+//Eventos de los Inputs y Textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
 }
 
+const nombre = document.querySelector("#nombre")
+const email = document.querySelector("#email")
+const mensaje = document.querySelector("#mensaje")
+const formulario = document.querySelector('.formulario')
+
+nombre.addEventListener('input', leerTexto)
+
+email.addEventListener('input', leerTexto)
+
+mensaje.addEventListener('input', leerTexto)
+
+// El evento de Submit
+
+formulario.addEventListener('submit', function(evento) {
+
+    evento.preventDefault();
+
+    //validar el formulario
+
+    const { nombre, email, mensaje }  = datos;
 
 
-console.log(5);
+    if( nombre === '' || email === '' || mensaje === '') {
+        mostrarError('todos los campos son obligatorios')
+
+        return; 
+    }
+
+    //Crear la otra alerta
+
+
+
+
+    //Enviar Formulario
+
+    console.log('Enviando formulario')
+}) 
+
+function leerTexto(e) {
+    // console.log(e.target.value)
+
+    datos[e.target.id] = e.target.value;
+  
+    // console.log(datos)
+}
+
+//Muestra un error en pantalla
+
+function mostrarError(mensaje){
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+    formulario.appendChild( error );
+    
+
+    // Desaparecer despues de 5 segundos
+
+    setTimeout ( () => {
+        error.remove();
+
+    }, 5000);
+
+}
 
 
 
